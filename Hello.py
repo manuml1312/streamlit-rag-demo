1417655f5cfcb5525bb21a812986f920fe9d8312
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-09 20:35:39 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-#import google.generativeai as palm
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.llms import GooglePalm
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
+
 OPENAI_API_KEY= st.secrets.openai_api
 
 def get_pdf_text(pdf_docs):
@@ -27,48 +25,43 @@ def get_text_chunks(text):
 
 def get_vector_store(text_chunks):
     embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    return vector_store
+    retriever = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return retriever
 
-def get_conversational_chain(vector_store):
-    llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY)
+def query_llm(retriever,query):
     memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
-    return conversation_chain
+    qa_chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(openai_api_key = OPENAI_API_KEY), 
+                                                               retriever=vector_store.as_retriever(), memory=memory)
+    result=qa_chain({"question":query,"chat_history":st.session_state.messages})
+    result=result['answer']
+    st.session_state.messages.append((query,result))
+    return result
 
-def user_input(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chatHistory = response['chat_history']
-    # for i, message in enumerate(st.session_state.chatHistory):
-    #     if i%2 == 0:
-    #         st.write("Human: ", message.content)
-    #     else:
-    #         st.write("Bot: ", message.content)
-def main():
-    st.set_page_config("Chat with Multiple PDFs")
-    st.header("LLM Powered Chatbot")
-    user_question = st.text_input("Ask a Question from the uploaded file")
-    # if "conversation" not in st.session_state:
-    #     st.session_state.conversation = None
-    # if "chatHistory" not in st.session_state:
-    #     st.session_state.chatHistory = None
-    if user_question:
-        user_input(user_question)
+def process_documents():
+    pdf_docs = st.session_state.source_docs
+    text= get_pdf_text(pdf_docs)
+    chunks=get_text_chunks(text)
+    st.session_state.retriever=get_vector_store(chunks)
+    
+def input_fields(): 
     with st.sidebar:
-        st.title("SoothsayerAnalytics")
-        #st.subheader("Upload your Documents Here")
-        pdf_docs = st.file_uploader("Upload Files and Click on the Process Button", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                vector_store = get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
-                for message in st.session_state.conversation:
-                    st.write({message['role']: message['content']})                
-                st.success("Done")
-            
+        st.session_state.source_docs=st.file_uploader(label="Upload the relevant documents for querying",accept_multiple_files=True)
+
+def boot():
+    input_fields()
+    st.sidebar:
+        st.button("Process",on_click=process_documents())
+    if "messages" not in st.session_state:
+        st.session_state.messages=[]
+    # for message in st.session_state.messages:
+    #     st.chat_message('human').write(message[0])
+    #     st.chat_message('ai').write(message[1])
+    if query:= st.chat_input():
+        st.chat_message("human").write(query)
+        response=query_llm(st.session_state.retriever,query)
+        st.chat_message("ai").write(response)
+    
 
 
 if __name__ == "__main__":
-    main()
+    boot()
