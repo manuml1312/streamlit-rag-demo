1e715be0a72c47e23712f5ca4abb3bbc0cfde7b5
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-09 11:21:09 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,31 +13,33 @@ st.title("📝 RAG - Demo ")
 
 with st.sidebar:
     uploaded_file=st.file_uploader("Upload the reference file to retrieve information",type=("txt","md","pdf"))
+
+if uploaded_file:
+    if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Ask me a question about any info within the document!"}
+        ]
+    if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+            st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+    if prompt :=st.text_input("Ask me any information from the document you uploaded",placeholder="Your query here",disabled=not uploaded_file):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
     
-llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="""You are an expert on the uploaded document and your job is to answer 
-          all questions. Assume that all questions are related to the document. Keep your answers accurate and based on 
+    llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="""You are an expert on the uploaded document and your job is to answer 
+              all questions. Assume that all questions are related to the document. Keep your answers accurate and based on 
                    facts retrieved from the document – do not hallucinate features.""")
 
-documents = uploaded_file.read().decode()
-service_context = ServiceContext.from_defaults(llm=llm) 
-index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+    documents = uploaded_file.read().decode()
+    service_context = ServiceContext.from_defaults(llm=llm) 
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about any info within the document!"}
-    ]
-    
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
-if prompt :=st.text_input("Ask me any information from the document you uploaded",placeholder="Your query here"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
 
 # If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = st.session_state.chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message)
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = st.session_state.chat_engine.chat(prompt)
+                st.write(response.response)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message)
