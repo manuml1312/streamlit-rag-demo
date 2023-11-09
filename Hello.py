25ddb8cae930d66f5533786e94ab4493a737b26b
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-09 11:23:30 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -19,11 +19,6 @@ if uploaded_file:
         st.session_state.messages = [
             {"role": "assistant", "content": "Ask me a question about any info within the document!"}
         ]
-    if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-            st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
-    if prompt :=st.text_input("Ask me any information from the document you uploaded",placeholder="Your query here",disabled=not uploaded_file):
-        st.session_state.messages.append({"role": "user", "content": prompt})
 
     
     llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="""You are an expert on the uploaded document and your job is to answer 
@@ -33,6 +28,12 @@ if uploaded_file:
     documents = uploaded_file.read().decode()
     service_context = ServiceContext.from_defaults(llm=llm) 
     index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+
+    if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+    if prompt :=st.text_input("Ask me any information from the document you uploaded",placeholder="Your query here",disabled=not uploaded_file):
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
 
 # If last message is not from assistant, generate a new response
