2fa5f1f0e9bbdb3d2099a6be8b707e264cd069be
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-09 19:45:56 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,40 +12,63 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 OPENAI_API_KEY= st.secrets.openai_api
 
-with st.sidebar:
-    pdf_docs = st.file_uploader("Upload Files and Click on the Process Button", accept_multiple_files=True)
-    if st.button("Process"):
-        with st.spinner("Processing"):
-            text=""
-            for pdf in pdf_docs:
-                pdf_reader= PdfReader(pdf)
-                for page in pdf_reader.pages:
-                    text+= page.extract_text()
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-chunks = text_splitter.split_text(text)
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
-vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+def get_vector_store(text_chunks):
+    embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return vector_store
 
-llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY)
-memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
-conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+def get_conversational_chain(vector_store):
+    llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY)
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
+
+def user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chatHistory = response['chat_history']
+    # for i, message in enumerate(st.session_state.chatHistory):
+    #     if i%2 == 0:
+    #         st.write("Human: ", message.content)
+    #     else:
+    #         st.write("Bot: ", message.content)
+def main():
+    st.set_page_config("Chat with Multiple PDFs")
+    st.header("LLM Powered Chatbot")
+    user_question = st.text_input("Ask a Question from the uploaded file")
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chatHistory" not in st.session_state:
+        st.session_state.chatHistory = None
+    if user_question:
+        user_input(user_question)
+    with st.sidebar:
+        st.title("SoothsayerAnalytics")
+        #st.subheader("Upload your Documents Here")
+        pdf_docs = st.file_uploader("Upload Files and Click on the Process Button", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vector_store = get_vector_store(text_chunks)
+                st.session_state.conversation = get_conversational_chain(vector_store)
+                st.success("Done")
+    with st.sidebar:
+        if st.button("Clear chat"):
+            
 
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = llm.as_chat_engine(chat_mode="condense_question", verbose=True)
-
-# if prompt := st.chat_input("Mention your requirements, based on which I can suggest materials",placeholder="Your processing queries here"): 
-
-if prompt :=st.text_input("How can i help you with your material processing query?",placeholder="Your Question Here"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-# If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = st.session_state.chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message)
+if __name__ == "__main__":
+    main()
