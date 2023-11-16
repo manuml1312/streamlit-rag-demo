217599695280ac733d1718883085869410a8ebb0
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-16 20:27:07 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,22 +1,19 @@
 import streamlit as st
-# from PyPDF2 import PdfReader
 from langchain.document_loaders import PDFMinerLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
-import os
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-import openai
 
-OPENAI_API_KEY= st.secrets.openai_api
+OPENAI_API_KEY = st.secrets.openai_api
 
 def get_pdf_text(pdf_docs):
-    text=""
-    pdf_reader= PDFMinerLoader(pdf_docs)
-    text=loader.load()
-    return  text
+    text = ""
+    pdf_reader = PDFMinerLoader(pdf_docs)
+    text = pdf_reader.load()
+    return text
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
@@ -24,47 +21,46 @@ def get_text_chunks(text):
     return chunks
 
 def get_vector_store(text_chunks):
-    embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY,request_timeout=120)
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY, request_timeout=120)
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     return vector_store
 
 def get_conversational_chain(vector_store):
-    llm = ChatOpenAI(openai_api_key = OPENAI_API_KEY)
-    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
     return conversation_chain
 
 def user_input(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chatHistory = response['chat_history']
-    for i, message in enumerate(st.session_state.chatHistory):
-        if i%2 == 0:
-            st.write("Human: ", message.content)
-        else:
-            st.write("Bot: ", message.content)
+    response = st.session_state.conversation_chain.chat({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        role = "Human" if i % 2 == 0 else "Bot"
+        st.write(f"{role}: {message.content}")
+
 def main():
     st.set_page_config("Chat with Multiple PDFs")
     st.header("LLM Powered Chatbot")
     user_question = st.text_input("Ask a Question from the uploaded file")
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chatHistory" not in st.session_state:
-        st.session_state.chatHistory = None
+    
+    if "conversation_chain" not in st.session_state:
+        st.session_state.conversation_chain = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     if user_question:
         user_input(user_question)
+
     with st.sidebar:
         st.title("SoothsayerAnalytics")
-        #st.subheader("Upload your Documents Here")
         pdf_docs = st.file_uploader("Upload Files and Click on the Process Button", type=["pdf"])
         if st.button("Process"):
             with st.spinner("Processing"):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
                 vector_store = get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
+                st.session_state.conversation_chain = get_conversational_chain(vector_store)
                 st.success("Done")
-
-
 
 if __name__ == "__main__":
     main()
